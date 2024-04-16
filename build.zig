d3fa1e56bab99d35042b2276e9ae926d7d818991
tiawl <pabtomas@protonmail.com>
2024-04-16 17:47:42 +0200

    [update] use toolbox 1.4.0

--- build.zig
+++ build.zig
@@ -2,9 +2,9 @@ const std = @import ("std");
 const toolbox = @import ("toolbox");
 const pkg = .{ .name = "shaderc.zig", .version = "2024.0.0", };
 
-fn update (builder: *std.Build, include_path: [] const u8) !void
+fn update (builder: *std.Build, shaderc_path: [] const u8) !void
 {
-  std.fs.deleteTreeAbsolute (include_path) catch |err|
+  std.fs.deleteTreeAbsolute (shaderc_path) catch |err|
   {
     switch (err)
     {
@@ -13,29 +13,34 @@ fn update (builder: *std.Build, include_path: [] const u8) !void
     }
   };
 
-  try toolbox.run (builder, .{ .argv = &[_][] const u8 { "git", "clone", "https://github.com/google/shaderc.git", include_path, }, });
-  try toolbox.run (builder, .{ .argv = &[_][] const u8 { "git", "-C", include_path, "checkout", "v" ++ pkg.version [0 .. pkg.version.len - 2], }, });
+  try toolbox.clone (builder, "https://github.com/google/shaderc.git",
+    "v" ++ pkg.version [0 .. pkg.version.len - 2], shaderc_path);
 
-  var include_dir = try std.fs.openDirAbsolute (include_path, .{ .iterate = true, });
-  defer include_dir.close ();
+  var shaderc_dir =
+    try std.fs.openDirAbsolute (shaderc_path, .{ .iterate = true, });
+  defer shaderc_dir.close ();
 
-  var it = include_dir.iterate ();
+  var it = shaderc_dir.iterate ();
   while (try it.next ()) |entry|
   {
     if (!std.mem.startsWith (u8, entry.name, "libshaderc"))
-      try std.fs.deleteTreeAbsolute (try std.fs.path.join (builder.allocator, &.{ include_path, entry.name, }));
+      try std.fs.deleteTreeAbsolute (try std.fs.path.join (builder.allocator,
+        &.{ shaderc_path, entry.name, }));
   }
 
-  var walker = try include_dir.walk (builder.allocator);
+  var walker = try shaderc_dir.walk (builder.allocator);
   defer walker.deinit ();
 
   while (try walker.next ()) |entry|
   {
-    if (entry.kind == .file and ((toolbox.is_cpp_source_file (entry.basename) and
+    if (entry.kind == .file and ((toolbox.isCppSource (
+      entry.basename) and
       std.mem.indexOf (u8, entry.basename, "test") != null) or
-      (!toolbox.is_cpp_source_file (entry.basename) and !toolbox.is_c_header_file (entry.basename)
-        and !std.mem.endsWith (u8, entry.basename, ".inc"))))
-          try std.fs.deleteFileAbsolute (try std.fs.path.join (builder.allocator, &.{ include_path, entry.path, }));
+      (!toolbox.isCppSource (entry.basename) and
+      !toolbox.isCHeader (entry.basename) and
+      !std.mem.endsWith (u8, entry.basename, ".inc"))))
+        try std.fs.deleteFileAbsolute (try std.fs.path.join (
+          builder.allocator, &.{ shaderc_path, entry.path, }));
   }
 
   var flag = true;
@@ -44,14 +49,15 @@ fn update (builder: *std.Build, include_path: [] const u8) !void
   {
     flag = false;
 
-    walker = try include_dir.walk (builder.allocator);
+    walker = try shaderc_dir.walk (builder.allocator);
     defer walker.deinit ();
 
     while (try walker.next ()) |entry|
     {
       if (entry.kind == .directory)
       {
-        std.fs.deleteDirAbsolute (try std.fs.path.join (builder.allocator, &.{ include_path, entry.path, })) catch |err|
+        std.fs.deleteDirAbsolute (try std.fs.path.join (builder.allocator,
+          &.{ shaderc_path, entry.path, })) catch |err|
         {
           if (err == error.DirNotEmpty) continue else return err;
         };
@@ -66,9 +72,11 @@ pub fn build (builder: *std.Build) !void
   const target = builder.standardTargetOptions (.{});
   const optimize = builder.standardOptimizeOption (.{});
 
-  const include_path = try builder.build_root.join (builder.allocator, &.{ "include", });
+  const shaderc_path =
+    try builder.build_root.join (builder.allocator, &.{ "shaderc", });
 
-  if (builder.option (bool, "update", "Update binding") orelse false) try update (builder, include_path);
+  if (builder.option (bool, "update", "Update binding") orelse false)
+    try update (builder, shaderc_path);
 
   const lib = builder.addStaticLibrary (.{
     .name = "shaderc",
@@ -77,7 +85,7 @@ pub fn build (builder: *std.Build) !void
     .optimize = optimize,
   });
 
-  var sources = try std.BoundedArray ([] const u8, 256).init (0);
+  const flags = [_][] const u8 { "-DENABLE_HLSL", "-fno-sanitize=undefined", };
 
   const glslang_dep = builder.dependency ("glslang", .{
     .target = target,
@@ -96,34 +104,30 @@ pub fn build (builder: *std.Build) !void
   lib.linkLibrary (spirv_compile_step);
   lib.installLibraryHeaders (spirv_compile_step);
 
-  for ([_] std.Build.LazyPath {
-    .{ .path = try std.fs.path.join (builder.allocator, &.{ "include", "libshaderc", "include", }), },
-    .{ .path = try std.fs.path.join (builder.allocator, &.{ "include", "libshaderc_util", "include", }), },
-  }) |include|
-  {
-    std.debug.print ("[shaderc include] {s}\n", .{ include.getPath (builder), });
-    lib.addIncludePath (include);
-  }
+  for ([_][] const u8 {
+    try std.fs.path.join (builder.allocator,
+      &.{ "shaderc", "libshaderc", "include", }),
+    try std.fs.path.join (builder.allocator,
+      &.{ "shaderc", "libshaderc_util", "include", }),
+  }) |include| toolbox.addInclude (lib, include);
 
-  const shaderc_path = try std.fs.path.join (builder.allocator, &.{ include_path, "libshaderc", });
-  const shaderc_include_path = try std.fs.path.join (builder.allocator, &.{ shaderc_path, "include", "shaderc", });
-  lib.installHeadersDirectory (.{ .path = shaderc_include_path, }, "shaderc", .{ .include_extensions = &.{ ".h", }, });
-  std.debug.print ("[shaderc headers dir] {s}\n", .{ shaderc_include_path, });
+  const libshaderc_path = try std.fs.path.join (builder.allocator,
+    &.{ shaderc_path, "libshaderc", });
+  toolbox.addHeader (lib, try std.fs.path.join (builder.allocator,
+    &.{ libshaderc_path, "include", "shaderc", }), "shaderc", &.{ ".h", });
 
-  const shaderc_util_path = try std.fs.path.join (builder.allocator, &.{ include_path, "libshaderc_util", });
-  const shaderc_util_include_path = try std.fs.path.join (builder.allocator, &.{ shaderc_util_path, "include", "libshaderc_util", });
-  lib.installHeadersDirectory (.{ .path = shaderc_util_include_path, }, "libshaderc_util", .{ .include_extensions = &.{ ".h", }, });
-  std.debug.print ("[shaderc headers dir] {s}\n", .{ shaderc_util_include_path, });
+  const libshaderc_util_path = try std.fs.path.join (builder.allocator,
+    &.{ shaderc_path, "libshaderc_util", });
+  toolbox.addHeader (lib, try std.fs.path.join (builder.allocator,
+    &.{ libshaderc_util_path, "include", "libshaderc_util", }),
+      "libshaderc_util", &.{ ".h", });
 
   var dir: std.fs.Dir = undefined;
   var walker: std.fs.Dir.Walker = undefined;
 
-  for ([_] struct { abs: [] const u8, rel: [] const u8, } {
-    .{ .abs = shaderc_path, .rel = try std.fs.path.join (builder.allocator, &.{ "include", "libshaderc", }), },
-    .{ .abs = shaderc_util_path, .rel = try std.fs.path.join (builder.allocator, &.{ "include", "libshaderc_util", }), },
-  }) |paths|
+  for ([_][] const u8 { libshaderc_path, libshaderc_util_path, }) |path|
   {
-    dir = try std.fs.openDirAbsolute (paths.abs, .{ .iterate = true, });
+    dir = try std.fs.openDirAbsolute (path, .{ .iterate = true, });
     defer dir.close ();
 
     walker = try dir.walk (builder.allocator);
@@ -134,21 +138,13 @@ pub fn build (builder: *std.Build) !void
       switch (entry.kind)
       {
         .file => {
-          if (toolbox.is_cpp_source_file (entry.basename))
-          {
-            try sources.append (try std.fs.path.join (builder.allocator, &.{ paths.rel, builder.dupe (entry.path), }));
-            std.debug.print ("[shaderc source] {s}\n", .{ try std.fs.path.join (builder.allocator, &.{ paths.abs, entry.path, }), });
-          }
+          if (toolbox.isCppSource (entry.basename))
+            try toolbox.addSource (lib, path, entry.path, &flags);
         },
         else => {},
       }
     }
   }
-
-  lib.addCSourceFiles (.{
-    .files = sources.slice (),
-    .flags = &.{ "-DENABLE_HLSL", "-fno-sanitize=undefined", },
-  });
 
   builder.installArtifact (lib);
 }
